e4d10a8f0cca29feb08b11eff2d12628c783dd8a
Tore Bergebakken <toberge@stud.ntnu.no>
2020-08-10 21:28:02 +0200

    Rewrite tokenizer and clean up a little

--- nessie.c
+++ nessie.c
@@ -4,8 +4,8 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define MAX_LENGTH 100
-#define MAX_TOKENS 20
+#define MAX_LENGTH 200
+#define MAX_TOKENS 30
 #define NUM_BUILTINS 1
 #define PATH_SIZE pathconf(".", _PC_PATH_MAX)
 
@@ -31,39 +31,50 @@ static int (*BUILTINS[NUM_BUILTINS]) (char[][MAX_LENGTH], int) = {
     cd
 };
 
+enum { OUTSIDE, IN_WORD, IN_STRING };
+
 int split_input(char tokens[][MAX_LENGTH]) {
     int i, n, c, state;
-    enum { OUTSIDE, IN_WORD, IN_STRING };
 
     i = 0, n = 0;
     state = OUTSIDE;
     while ((c = getchar()) != EOF && c != '\n') {
-        if (c == ' '  && state == IN_WORD) {
-            state = OUTSIDE;
-            /* tokens[n] = token; */
-            tokens[n][i] = '\0';
-            /* char newtoken[MAX_LENGTH]; */
-            /* token = newtoken; // okay whatever */
-            n++;
-            i = 0;
-        } else if (c == '"') {
-            if (state == OUTSIDE) {
-                state = IN_STRING;
-            } else if (state == IN_STRING) {
-                state = OUTSIDE;
-                tokens[n][i] = '\0';
-                n++;
-                i = 0;
-            } else {
-                state = IN_WORD;
-                /* token[i] = c; */
-                tokens[n][i] = c;
-                i++;
-            }
-        } else {
-            if (state != IN_STRING) state = IN_WORD;
-            tokens[n][i] = c;
-            i++;
+        if (n > MAX_TOKENS || i > MAX_LENGTH) {
+            printf("Max amount of tokens, or chars in token, exceeded!\n");
+            while (getchar() != '\n')
+                ;
+            return -1;
+        }
+
+        switch (state) {
+            case OUTSIDE:
+                if (c == '"') {
+                    state = IN_STRING;
+                } else if (c != ' ') {
+                    state = IN_WORD;
+                    tokens[n][i++] = c;
+                }
+                break;
+            case IN_WORD:
+                if (c == ' ') {
+                    state = OUTSIDE;
+                    tokens[n++][i] = '\0';
+                    i = 0;
+                } else {
+                    tokens[n][i++] = c;
+                }
+                break;
+            case IN_STRING:
+                if (c == '"') { // TODO: check if whitespace follows
+                    state = OUTSIDE;
+                    tokens[n++][i] = '\0';
+                    i = 0;
+                } else {
+                    tokens[n][i++] = c;
+                }
+                break;
+            default:
+                printf("Invalid state\n");
         }
     }
     if (i) {
@@ -109,7 +120,7 @@ int execute_command(char tokens[][MAX_LENGTH], int argc) {
         status = execvp(tokens[0], (char * const*) argv);
     } else {
         // negative pid -> error forking
-        perror("Error while forking!");
+        printf("Error while forking!\n");
         status = -1;
     }
 
@@ -121,7 +132,7 @@ int main() {
     int count, status = 0, debug = 0, exit_status = 0;
 
     char *cwd;
-    char *cwd_buffer = (char*)malloc((size_t)PATH_SIZE);
+    char *cwd_buffer = (char*) malloc((size_t)PATH_SIZE);
 
     printf("Welcome to ne[sh]ie, the absurdly stupid shell!\n");
 
@@ -133,8 +144,12 @@ int main() {
         else
             printf("%s \033[33m$\033[0m ", cwd);
 
+        status = -1;
+
         // Read and split input
         count = split_input(tokens);
+        if (count < 0)
+            continue;
         if (debug) {
             printf("Read %i tokens: ", count);
             printarr(tokens, count);
@@ -148,6 +163,9 @@ int main() {
             }
         }
 
+        if (status != -1) // cmd was a builtin
+            continue;
+
         // Handle exit
         if (strcmp(tokens[0], "exit") == 0)
             break;
@@ -159,10 +177,11 @@ int main() {
         if (status < 0) {
             exit_status = 1;
             break;
-        } else if (chdir(tokens[0]) == 0)
+        } else if (chdir(tokens[0]) == 0) {
             status = 0; // since it's not a command, try cd-ing with it!
-        else if (status == 256)
+        } else if (status == 256) {
             printf("No such command or directory: %s\n", tokens[0]);
+        }
     }
 
     free(cwd_buffer);
