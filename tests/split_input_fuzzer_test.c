8c67c6c05e9c4571fbb5cca8e577245b28507d6d
Tore Bergebakken <toberge@stud.ntnu.no>
2020-10-11 20:32:29 +0200

    Fix fuzzer test and constrain it further
    - Fixed extra `len` parameter to split_input...
    - There will be no null bytes _inside_ the string, yo

--- tests/split_input_fuzzer_test.c
+++ tests/split_input_fuzzer_test.c
@@ -3,16 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-char **split_input(const char *input, const int len, int *num_tokens);
+char **split_input(const char *input, int *num_tokens);
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-    // constrain to null-terminated strings
+    // constrain to strings w/o null bytes inside
+    for (int i = 0; i < size; i++)
+        if (data[i] == '\0')
+            return 0; // gotcha!
+
+    // make all inputs null-terminated strings
     char *instring = malloc(sizeof(char) * (size + 1));
     memcpy(instring, data, size); // copy memory chunk
     instring[size] = '\0'; // null-terminate
-    
+
     int num_tokens;
-    char **tokens = split_input(instring, size, &num_tokens);
+    char **tokens = split_input(instring, &num_tokens);
     for (int i = 0; i < num_tokens; i++)
         free(tokens[i]);
     free(tokens);
