toberge/nessie

commit 6a2e9128b503dca697658b1473ab5333fb596eec

Fix memory leak in lexer
One bug down, two more to go...
Freeing any token if i == 0, preventing leak of 64 bytes.
Freeing and returning NULL if n == 0, to possibly fix some behaviour.

lexer.c
@@ -105,7 +105,12 @@ char **split_input(const char *input, int *num_tokens) {
         n++;
     } else if (!n) { // no tokens acquired
         free(tokens[0]);
+        free(tokens);
+        *num_tokens = 0;
+        return NULL;
+    } else if (!i) { // last tokens has size 0
+        free(tokens[n--]);
     }
     *num_tokens = n;
-    return tokens;
+    return realloc(tokens, n*sizeof(char*));
 }

commit c4e0f2f3f4ed47b8b65ba1f8de25dd2e91676004

Fix regression caused by 6a2e9128
Commit 6a2e9128 introduced a bug where the last token would be freed and
discarded *if it was a string*, since encountering the end of a string
resets the char index and the if-statement that triggered thee freeing
*just* checked if the index was 0.

THIS MIGHT REINTRODUCE A MEMORY LEAK.

lexer.c
@@ -167,9 +167,7 @@ char **split_input(const char *input, int *num_tokens) {
         free(tokens);
         *num_tokens = 0;
         return NULL;
-    } else if (!i) { // last token has size 0
-        free(tokens[n--]);
-    }
+    } // TODO: Last token might be empty, is that an issue?
     *num_tokens = n;
     return tokens;
 }